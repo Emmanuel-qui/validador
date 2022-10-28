ef16229679ad49e4b76c8255fb6c18f45d693900
Emmanuel-qui <emmanuel.quiroz1223@gmail.com>
2022-10-28 18:02:12 -0500

    Agregue funcionalidad a los filtros de busqueda

--- static/js/result.js
+++ static/js/result.js
@@ -9,8 +9,19 @@ $(document).ready(function () {
             data: (d) => {
                 d.csrfmiddlewaretoken = csrftoken;
                 var rfc_emisor = $("#filter_rfc_emisor").val();
+                var rfc_receptor = $("#filter_rfc_receptor").val();
+                var fecha_validacion =$("#filter_fecha").val();
                 if (rfc_emisor) {
                     d.rfc_emisor = rfc_emisor;
+                }
+
+                if(rfc_receptor){
+                    d.rfc_receptor = rfc_receptor;
+                }
+
+                if(fecha_validacion){
+                    d.fecha_validacion = fecha_validacion;
+                    
                 }
             },
         },
@@ -25,11 +36,19 @@ $(document).ready(function () {
         ],
     });
 
-    $("#id_get").on("click", function() {
-        datatable.ajax.reload();
-    })
-
     $("#filter_rfc_emisor").on("keyup", function() {
         datatable.ajax.reload();
-    })
+    });
+
+    $("#filter_rfc_receptor").on("keyup", function(){
+        datatable.ajax.reload();
+    });
+
+    
+    
+    $("#filter_fecha").on("change", function(){
+        datatable.ajax.reload();
+    });
+
+   
 });
